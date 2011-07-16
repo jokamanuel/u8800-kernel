3fc25693662c1d0531c7edb1da390a28564bbbde
francisco <francisco@francisco.(none)>
2011-07-16 06:16:55 +0100

    writeback: balance_dirty_pages(): reduce calls to global_page_state

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 23 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 PREEMPT Sat Jul 16 05:12:45 WEST 2011"
-#define LINUX_COMPILE_TIME "05:12:45"
+#define UTS_VERSION "#23 PREEMPT Sat Jul 16 06:11:46 WEST 2011"
+#define LINUX_COMPILE_TIME "06:11:46"
 #define LINUX_COMPILE_BY "francisco"
 #define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILE_DOMAIN
