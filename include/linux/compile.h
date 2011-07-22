ea186f739ddae66c2ab4e33c99d8842f4188317f
francisco <francisco@francisco.(none)>
2011-07-22 06:21:04 +0100

    Add ZRAM

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sat Jul 16 06:53:23 WEST 2011"
-#define LINUX_COMPILE_TIME "06:53:23"
+#define UTS_VERSION "#2 PREEMPT Fri Jul 22 06:04:22 WEST 2011"
+#define LINUX_COMPILE_TIME "06:04:22"
 #define LINUX_COMPILE_BY "francisco"
 #define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILE_DOMAIN
