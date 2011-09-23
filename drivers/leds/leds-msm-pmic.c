0ff6d4f8a78c507f6c80472bd4c3c5ce3f6f8180
franciscofranco <franciscofranco.1990@gmail.com>
2011-09-23 01:57:19 +0100

    Disable keypad leds from sysfs - from stockwell - getting ready for his next miui release

--- drivers/leds/leds-msm-pmic.c
+++ drivers/leds/leds-msm-pmic.c
@@ -32,6 +32,9 @@
 #include <asm/mach-types.h>
 #endif
 #define MAX_KEYPAD_BL_LEVEL	16
+static int disable_keypad_leds = 0;
+
+module_param(disable_keypad_leds,int,00644);
 
 static void msm_keypad_bl_led_set(struct led_classdev *led_cdev,
 	enum led_brightness value)
@@ -41,7 +44,8 @@ static void msm_keypad_bl_led_set(struct led_classdev *led_cdev,
     if(machine_is_msm7x30_u8800()) 
     {
 
-      ret = pmic_set_led_intensity(LED_KEYPAD, value / LED_FULL);
+	if(disable_keypad_leds) value = 0;
+      	  ret = pmic_set_led_intensity(LED_KEYPAD, value / LED_FULL);
     }
         
     if(machine_is_msm7x30_u8820())
